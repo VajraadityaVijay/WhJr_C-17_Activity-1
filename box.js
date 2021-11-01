491197d7f93f3d78fd9fd6cef3f5ffeec031c3e3
Vajraaditya Vijay <88263017+VajraadityaVijay@users.noreply.github.com>
2021-11-01 19:45:22 +0530

    First Commit

---
+++ box.js
@@ -0,0 +1,29 @@
+class Box
+  {
+    constructor(x,y,w,h,vx,vy)
+    {
+      this.x =x;
+      this.y =y;
+      this.w =w;
+      this.h = h;
+      this.vx = vx;
+      this.vy = vy;
+    }
+    
+    show()
+    {
+      rect(this.x,this.y,this.w,this.h)
+    }
+    
+    moveX()
+    {
+      this.x = this.x+this.vx;
+    }
+
+    moveY()
+    {
+      this.y = this.y + this.vy;
+    }
+  }
+
+  
